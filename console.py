692a0bdb696a012325fe8b1798f724231e6a461d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-10 12:46:22 +0000

    Updated

--- console.py
+++ console.py
@@ -3,11 +3,14 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from shlex import split
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """Cmd"""
     prompt = '(hbnb) '
+    class_name = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -25,105 +28,87 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Create a new instance of BaseModel, save it, and print the id"""
-        if not arg:
+        arguments = split(arg)
+        if len(arguments) == 0:
             print("** class name missing **")
-            return
-        try:
-            new_instance = eval(arg)()
+        elif arguments[0] not in self.class_name:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(f"{arguments[0]}()")
             new_instance.save()
             print(new_instance.id)
-        except NameError:
-            print("** class doesn't exist **")
 
     def do_show(self, arg):
-        """Prints the string representation of an instance"""
-        args = arg.split()
-        if not args:
+        """Show command to display the string representation of an instance"""
+        arguments = split(arg)
+        if len(arguments) == 0:
             print("** class name missing **")
-            return
-        try:
-            if args[0] not in storage.classes():
-                print("** class doesn't exist **")
-                return
-            if len(args) < 2:
-                print("** instance id missing **")
-                return
-            obj_key = "{}.{}".format(args[0], args[1])
-            if obj_key not in storage.all():
+        elif arguments[0] not in self.class_name:
+            print("** class doesn't exist **")
+        elif not arguments[1]:
+            print("** instance id missing **")
+        else:
+            instance_key = f"{arguments[0]}.{arguments[1]}"
+            if instance_key in storage.all():
+                print(storage.all()[instance_key])
+            else:
                 print("** no instance found **")
-                return
-            print(storage.all()[obj_key])
-        except:
-            pass
 
     def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        args = arg.split()
-        if not args:
+        """Destroy command to delete an instance based on class name and id"""
+        arguments = split(arg)
+        if len(arguments) == 0:
             print("** class name missing **")
-            return
-        try:
-            if args[0] not in storage.classes():
-                print("** class doesn't exist **")
-                return
-            if len(args) < 2:
-                print("** instance id missing **")
-                return
-            obj_key = "{}.{}".format(args[0], args[1])
-            if obj_key not in storage.all():
+        elif arguments[0] not in self.class_name:
+            print("** class doesn't exist **")
+        elif not arguments[1]:
+            print("** instance id missing **")
+        else:
+            instance_key = f"{arguments[0]}.{arguments[1]}"
+            if instance_key in storage.all():
+                del storage.all()[instance_key]
+                storage.save()
+            else:
                 print("** no instance found **")
-                return
-            del storage.all()[obj_key]
-            storage.save()
-        except:
-            pass
 
     def do_all(self, arg):
-        """Prints all string representation of all instances"""
-        args = arg.split()
-        try:
-            if args and args[0] not in storage.classes():
-                print("** class doesn't exist **")
-                return
-            result = [str(value) for key, value in storage.all().items()]
-            print(result)
-        except:
-            pass
+        """"""
+        arguments = split(arg)
+        if len(arguments) == 0:
+            instances_list = [str(value) for value in storage.all().values()]
+            print(instances_list)
+        elif arguments[0] not in self.class_name:
+            print("** class doesn't exist **")
+        else:
+            instances_list = [
+                    str(value)
+                    for key, value in storage.all().items()
+                    if arguments[0] in key
+                    ]
+            print(insances_list)
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id"""
-        args = arg.split()
-        try:
-            if not args:
-                print("** class name missing **")
-                return
-            if args[0] not in storage.classes():
-                print("** class doesn't exist **")
-                return
-            if len(args) < 2:
-                print("** instance id missing **")
-                return
-            obj_key = "{}.{}".format(args[0], args[1])
-            if obj_key not in storage.all():
-                print("** no instance found **")
-                return
-            if len(args) < 3:
-                print("** attribute name missing **")
-                return
-            if len(args) < 4:
-                print("** value missing **")
-                return
-            obj_instance = storage.all()[obj_key]
-            attr_name = args[2]
-            attr_value = args[3]
-            try:
-                attr_value = eval(attr_value)
-            except:
-                pass
-            setattr(obj_instance, attr_name, attr_value)
-            storage.save()
-        except:
-            pass
+        arguments = split(arg)
+        if len(arguments) == 0:
+            print("** class name missing **")
+        elif arguments[0] not in self.class_name:
+            print("** class doesn't exist **")
+        elif len(arguments) == 1:
+            print("** instance id missing **")
+        elif f"{arguments[0]}.{arguments[1]}" not in storage.all():
+            print("** no instance found **")
+        elif len(arguments) == 2:
+            print("** attribute name missing **")
+        elif len(arguments) == 3:
+            print("** value missing **")
+        else:
+            instance_key = f"{arguments[0]}.{arguments[1]}"
+            instance = storage.all()[instance_key]
+            attr_name = arguments[2]
+            attr_value = arguments[3]
+            setattr(instance, attr_name, attr_value)
+            instance.save()
 
 
 if __name__ == '__main__':
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Test for User"""
+import unittest
+from models.user import User
+from datetime import datetime
+from models import storage
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """Set up for testing."""
+        self.user = User()
+
+    def tearDown(self):
+        """Clean test files."""
+        pass
+
+    def test_attributes(self):
+        """Test user attributes."""
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'password'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+
+    def test_default_values(self):
+        """Test default values."""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_save_reload(self):
+        """Test if User saves and reloads correctly."""
+        self.user.email = "test@example.com"
+        self.user.password = "password123"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+        self.user.save()
+        key = "{}.{}".format(self.user.__class__.__name__, self.user.id)
+        reloaded_user = storage.all()[key]
+        self.assertEqual(reloaded_user.email, "test@example.com")
+        self.assertEqual(reloaded_user.password, "password123")
+        self.assertEqual(reloaded_user.first_name, "John")
+        self.assertEqual(reloaded_user.last_name, "Doe")
+
+    def test_to_dict(self):
+        """Test if to_dict method produces correct dictionary."""
+        self.user.email = "test@example.com"
+        self.user.password = "password123"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+        user_dict = self.user.to_dict()
+        self.assertEqual(user_dict['email'], "test@example.com")
+        self.assertEqual(user_dict['password'], "password123")
+        self.assertEqual(user_dict['first_name'], "John")
+        self.assertEqual(user_dict['last_name'], "Doe")
+        self.assertEqual(user_dict['__class__'], "User")
+        self.assertTrue('id' in user_dict)
+        self.assertTrue('created_at' in user_dict)
+        self.assertTrue('updated_at' in user_dict)
+
+    def test_created_at_updated_at(self):
+        """Test if created_at and updated_at are instances of datetime."""
+        self.assertTrue(isinstance(self.user.created_at, datetime))
+        self.assertTrue(isinstance(self.user.updated_at, datetime))
+
+
+if __name__ == "__main__":
+    unittest.main()
