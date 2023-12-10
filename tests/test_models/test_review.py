0dd6927a4115c83ef696f174a1a9b166145c8fb6
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-10 16:08:33 +0000

    Updated

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""test"""
-
+"""Unit tests for the Review class"""
 import unittest
 from models.review import Review
 from datetime import datetime
@@ -8,56 +7,56 @@ from models import storage
 
 
 class TestReview(unittest.TestCase):
-    """test"""
+    """Test cases for the Review class"""
 
     def setUp(self):
-        """test"""
+        """Set up a Review instance for testing"""
         self.review = Review()
 
     def tearDown(self):
-        """test"""
+        """Clean up after testing"""
         pass
 
     def test_attributes(self):
-        """test"""
+        """Test the existence of required attributes"""
         self.assertTrue(hasattr(self.review, 'place_id'))
         self.assertTrue(hasattr(self.review, 'user_id'))
         self.assertTrue(hasattr(self.review, 'text'))
 
     def test_default_values(self):
-        """test"""
+        """Test default values of Review attributes"""
         self.assertEqual(self.review.place_id, "")
         self.assertEqual(self.review.user_id, "")
         self.assertEqual(self.review.text, "")
 
     def test_save_reload(self):
-        """test"""
+        """Test saving and reloading a Review instance"""
         self.review.place_id = "123"
         self.review.user_id = "456"
-        self.review.text = "Great experience!"
+        self.review.text = "Fantastic stay!"
         self.review.save()
         key = "{}.{}".format(self.review.__class__.__name__, self.review.id)
         reloaded_review = storage.all()[key]
         self.assertEqual(reloaded_review.place_id, "123")
         self.assertEqual(reloaded_review.user_id, "456")
-        self.assertEqual(reloaded_review.text, "Great experience!")
+        self.assertEqual(reloaded_review.text, "Fantastic stay!")
 
     def test_to_dict(self):
-        """test"""
+        """Test conversion of Review instance to dictionary"""
         self.review.place_id = "789"
         self.review.user_id = "012"
-        self.review.text = "Amazing view from the balcony"
+        self.review.text = "Lovely place, highly recommended"
         review_dict = self.review.to_dict()
         self.assertEqual(review_dict['place_id'], "789")
         self.assertEqual(review_dict['user_id'], "012")
-        self.assertEqual(review_dict['text'], "Amazing view from the balcony")
+        self.assertEqual(review_dict['text'], "Lovely place, highly recommended")
         self.assertEqual(review_dict['__class__'], "Review")
         self.assertTrue('id' in review_dict)
         self.assertTrue('created_at' in review_dict)
         self.assertTrue('updated_at' in review_dict)
 
     def test_created_at_updated_at(self):
-        """test"""
+        """Test the data types of created_at and updated_at attributes"""
         self.assertTrue(isinstance(self.review.created_at, datetime))
         self.assertTrue(isinstance(self.review.updated_at, datetime))
 
